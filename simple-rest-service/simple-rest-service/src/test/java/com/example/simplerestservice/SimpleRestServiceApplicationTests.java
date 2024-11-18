8709c321c9fe9ea0de858a46d39909b43ae7cc97
Vedant Mehta <95914389+vedantmehta14@users.noreply.github.com>
2024-11-18 15:45:12 -0800

    Added all the folders

---
+++ simple-rest-service/simple-rest-service/src/test/java/com/example/simplerestservice/SimpleRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplerestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
